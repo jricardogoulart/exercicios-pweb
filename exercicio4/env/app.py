153d3c580417d498e460a12cfa435db69f2a087d
José Ricardo Goulart Silva <131194841+jricardogoulart@users.noreply.github.com>
2023-10-11 12:21:30 -0300

    "Organizando arquivos e adicionando exercicio 4"

---
+++ exercicio4/env/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request,redirect, url_for
+
+app = Flask(__name__)
+usuarios_permitidos = {"admin": "123"}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username in usuarios_permitidos and usuarios_permitidos[username] == password:
+        return redirect(url_for('sucesso'))
+    else:
+        return redirect(url_for('erro'))
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/sucesso')
+def sucesso():
+    return render_template('login.html', titulo="Lista dos Clientes")
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    alert_message = "Usuário Cadastrado com Sucesso "  # Defina a mensagem que deseja exibir no alerta.
+    return render_template('alert.html', alert_message=alert_message)
+    
+    
+
+@app.route('/erro')
+def erro():
+    return "Usuário ou senha incorretos. Tente novamente."
+#ChatGPT tá Salvando essas Aulas
+    
+if __name__ == '__main__':
+    app.run(debug=True)
