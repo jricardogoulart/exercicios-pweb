0b5042e3c795d7f7f96db13808363850db931dfb
José Ricardo Goulart Silva <131194841+jricardogoulart@users.noreply.github.com>
2023-10-17 00:47:41 -0300

    Correção Exercício 2 + Integração com BD

--- exercicio2/env/app.py
+++ exercicio2/env/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request,redirect, url_for
 
+
 app = Flask(__name__)
 usuarios_permitidos = {"admin": "123"}
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+
+@app.route('/login')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -20,13 +31,14 @@ def login():
 
 @app.route('/sucesso')
 def sucesso():
-    return render_template('login.html')
+    return render_template('tabela.html')
     
+
 
 @app.route('/erro')
 def erro():
     return "Usuário ou senha incorretos. Tente novamente."
-#ChatGPT tá Salvando essas Aulas
+
     
 if __name__ == '__main__':
     app.run(debug=True)
